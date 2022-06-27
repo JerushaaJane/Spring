9011243bb69a0cb514852ab6db54ed128737181f
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2022-06-27 18:06:59 +0530

    Add files via upload

--- spring1/src/main/java/com/example/main/main.java
+++ spring1/src/main/java/com/example/main/main.java
@@ -7,21 +7,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class main {
     public static void main(String[] args) {
 
-        Vehicle v=new Vehicle();
-        v.setName("Tata Nexus");
-        System.out.println("Vehicle name from a non spring context: "+v.getName());
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
-        Vehicle vname=context.getBean(Vehicle.class);
-        System.out.println("Vehicle name from a spring context: "+vname.getName());
 
         String owner=context.getBean(String.class);
         System.out.println("Owner name from a spring context: "+owner);
 
         //To overcome NoUniqueBeanException
-       // Vehicle v2=context.getBean("vehicle2",Vehicle.class);
-       // System.out.println("Vehicle 2 of owner: "+v2.getName());
+        Vehicle v2=context.getBean("vehicle2",Vehicle.class);   //vehicle2 is the bean name
+        System.out.println("Vehicle 2 of owner: "+v2.getName());
+
+        Vehicle v3=context.getBean("vehicle3",Vehicle.class);    //vehicle3 is the bean name
+        System.out.println("Vehicle 3 of owner: "+v3.getName());
+
+        Vehicle v=context.getBean("m1",Vehicle.class);   //m1 is the bean name
+        System.out.println("Vehicle 1 of owner: "+v.getName());
+
+
+
 
     }
 }
